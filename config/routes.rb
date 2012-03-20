95bb647d9eca330e34f4f414b55f5108227e22bd
Carl Ehrnrooth <carl.ehrnrooth@gmail.com>
2012-03-20 15:13:09 -0700

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ DemoApp::Application.routes.draw do
 
   resources :users
   
-  root :to => 'users#index'
+  root :to => 'user'
   
-  match '/user', :to => '/users'
-  match '/micropost', :to => '/microposts'
+  match '/user', :to => 'user'
+  match '/micropost', :to => 'micropost'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
